1f7b08d2be6d9d9ff9a1c15acc825143e76995ed
(Fallox) Nikko Renolds <ni1kko@outlook.com>
2022-01-18 01:07:02 +0000

    Created lib

---
+++ StayALiVE Lib/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StayALiVE_Lib
+{
+    public class Core
+    {
+        public const string test = "StayALiVE_Lib\n";
+    }
+}
